54c9a61b8c8854ba02466480b8d2a27d31df7233
Brian Gluzman <bgluzman@gmail.com>
2017-09-26 00:19:54 -0400

    Move grammar defn into seperate file

---
+++ src/grammar.cpp
@@ -0,0 +1,34 @@
+#include <grammar.h>
+
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/support.hpp>
+
+namespace score
+{
+    namespace qi = boost::spirit::qi;
+    namespace ascii = boost::spirit::ascii;
+
+    template <typename Iterator> struct Grammar
+      : qi::grammar<Iterator, ast::Statement(), ascii::space_type>
+    {
+      Grammar() : Grammar::base_type(statementRule)
+      {
+        // TODO
+      }
+
+      qi::rule<Iterator, ast::Statement(), ascii::space_type> statementRule;
+    };
+
+    bool parse(ast::Statement& out, const std::string& in)
+    {
+      using boost::spirit::ascii::space;
+
+      score::Grammar<std::string::const_iterator> grammar;
+
+      const auto& iter = in.cbegin();
+      const auto& end  = in.cend();
+      bool parsed = phrase_parse(iter, end, grammar, space, out);
+
+      return parsed;
+    }
+}
---
+++ src/grammar.h
@@ -0,0 +1,11 @@
+#include <string>
+
+namespace score { namespace ast {
+  struct Statement { };
+}
+}
+
+namespace score
+{
+  bool parse(ast::Statement& out, const std::string& in);
+}
